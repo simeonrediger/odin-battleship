f24c6807b6efd8f18a0ec8bcc431ee1c00e2c7ab
Simeon Rediger <simeonrediger@gmail.com>
2026-01-10 20:57:17 -0500

    Introduce handleGameConfigSubmit()

--- src/menu/controller.js
+++ src/menu/controller.js
@@ -1,8 +1,10 @@
 import gameConfigView from './game-config/view.js';
 
 function init(root) {
-    gameConfigView.render(root);
+    gameConfigView.render(root, handleGameConfigSubmit);
 }
+
+function handleGameConfigSubmit({ player1Data, player2Data }) {}
 
 const menuController = {
     init,
