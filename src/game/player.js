91270ea3a08aee1dabc5e43c55f0e75ec2f03bef
Simeon Rediger <simeonrediger@gmail.com>
2026-01-10 21:50:56 -0500

    Add isHuman property

--- src/game/player.js
+++ src/game/player.js
@@ -1,14 +1,22 @@
 export default class Player {
     #name;
+    #isHuman;
 
-    constructor(name) {
-        this.#validateArgs(name);
+    constructor(name, isHuman) {
+        this.#validateArgs(name, isHuman);
         this.#name = name;
+        this.#isHuman = isHuman;
     }
 
-    #validateArgs(name) {
+    #validateArgs(name, isHuman) {
         if (typeof name !== 'string') {
             throw new TypeError(`Name must be a string. Got ${typeof name}`);
+        }
+
+        if (typeof isHuman !== 'boolean') {
+            throw new TypeError(
+                `isHuman must be a boolean. Got ${typeof isHuman}`,
+            );
         }
     }
 }
--- tests/player.test.js
+++ tests/player.test.js
@@ -1,6 +1,11 @@
 import Player from '../src/game/player.js';
 
 test('only accepts string names', () => {
-    expect(() => new Player('abc')).not.toThrow();
-    expect(() => new Player(5)).toThrow();
+    expect(() => new Player('abc', true)).not.toThrow();
+    expect(() => new Player(5, true)).toThrow();
+});
+
+test('only accepts boolean isHuman value', () => {
+    expect(() => new Player('abc', true)).not.toThrow();
+    expect(() => new Player('abc', 'true')).toThrow();
 });
