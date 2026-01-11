7fe584a8f99c538fceb6005711491a4cadeada6d
Simeon Rediger <simeonrediger@gmail.com>
2026-01-10 21:31:03 -0500

    Move shared and static styles into shared/

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import './styles/reset.css';
-import './styles/colors.css';
-import './styles/layout.css';
+import '@/shared/styles/reset.css';
+import '@/shared/styles/colors.css';
+import '@/shared/styles/layout.css';
 
 import gameController from './game/controller.js';
 import menuController from './menu/controller.js';
