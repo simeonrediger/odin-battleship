4d53d85ee5fbce5363f107f189104e063b6f29aa
Simeon Rediger <simeonrediger@gmail.com>
2026-01-18 03:07:12 -0500

    Refactor to use callbacks to drive the game

--- src/game/driver.js
+++ src/game/driver.js
@@ -1,11 +1,16 @@
-function play(game) {
-    game.start();
-    game.submitPlayerCreation(true, 'P1', true, 'P2');
-}
+let game;
 
 const driver = {
-    play,
-    handleEnterPlayerCreation: () => console.log('Entered player creation'),
+    play: gameModel => {
+        game = gameModel;
+        game.start();
+    },
+
+    handleEnterPlayerCreation: () => {
+        console.log('Entered player creation');
+        game.submitPlayerCreation(true, 'P1', true, 'P2');
+    },
+
     handlePlayerChange: name => console.log('Current player:', name),
     handleEnterShipPlacements: shipIds =>
         console.log('Entered ship placements. Received:', shipIds),
