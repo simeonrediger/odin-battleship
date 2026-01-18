d2fc85a47ed1948a95417a13ce6c86f74a0dd9ec
Simeon Rediger <simeonrediger@gmail.com>
2026-01-17 22:31:11 -0500

    Enter player creation phase

--- src/game/game.js
+++ src/game/game.js
@@ -1,4 +1,18 @@
-function init() {}
+const phases = Object.freeze({
+    PLAYER_CREATION: 'PLAYER_CREATION',
+});
+
+const current = {
+    phase: undefined,
+};
+
+function init() {
+    enterPlayerCreation();
+}
+
+function enterPlayerCreation() {
+    current.phase = phases.PLAYER_CREATION;
+}
 
 const game = {
     init,
