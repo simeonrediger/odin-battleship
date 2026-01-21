7052088ce6e8c8dd6963ec5aff0d8c6e5c7151bd
Simeon Rediger <simeonrediger@gmail.com>
2026-01-21 15:58:57 -0500

    Import global styles

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import './shared/styles/reset.css';
+import './shared/styles/colors.css';
+import './shared/styles/layout.css';
+
 import gameDriver from './domains/game/game-driver.js';
 
 gameDriver.start(document);
