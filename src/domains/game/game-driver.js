24fe85571d8ae8af01d36b984cf3b2433a6b40f1
Simeon Rediger <simeonrediger@gmail.com>
2026-01-21 22:16:42 -0500

    Remove and relocate unnecessary methods from API
    Removes certain methods from the module's API and converts them to
    functions in the module scope.

--- src/domains/game/game-driver.js
+++ src/domains/game/game-driver.js
@@ -9,70 +9,70 @@ let x1 = 0,
     x2 = 0,
     y2 = 0;
 
+function start(root) {
+    game.init({
+        onEnterPlayerCreation: handleEnterPlayerCreation,
+        onPlayerChange: handlePlayerChange,
+        onEnterShipPlacements: handleEnterShipPlacements,
+        onEnterRound: handleEnterRound,
+        onDeclareWinner: handleDeclareWinner,
+    });
+
+    const gameContainer = root.querySelector("[data-role='game-container']");
+
+    gameView.init(gameContainer);
+    game.start();
+}
+
+function handleEnterPlayerCreation() {
+    if (gameCompleted) {
+        console.log('Game restarted');
+        return;
+    }
+
+    gameView.showPlayerCreation();
+    game.submitPlayerCreation(true, 'P1', true, 'P2');
+}
+
+function handlePlayerChange(name) {
+    console.log('Current player:', name);
+    player = name === 'P1' ? 1 : 2;
+}
+
+function handleEnterShipPlacements(shipIds) {
+    console.log('Entered ship placements. Received:', shipIds);
+    game.placeShip(shipIds[0].id, 4, 3);
+    game.placeShip(shipIds[1].id, 4, 4);
+    game.placeShip(shipIds[2].id, 4, 5);
+    game.placeShip(shipIds[3].id, 4, 6);
+    game.placeShip(shipIds[4].id, 4, 7);
+    game.submitShipPlacements();
+}
+
+function handleEnterRound() {
+    console.log('Entered round');
+
+    if (player === 1) {
+        y1 = x1 < 10 ? y1 : y1 + 1;
+        x1 = x1 < 10 ? x1 : 0;
+        console.log(`Submitting attack to (${x1}, ${y1})`);
+        game.submitAttack(x1++, y1);
+    } else {
+        y2 = x2 < 10 ? y2 : y2 + 1;
+        x2 = x2 < 10 ? x2 : 0;
+        console.log(`Submitting attack to (${x2}, ${y2})`);
+        game.submitAttack(x2++, y2);
+    }
+}
+
+function handleDeclareWinner(name) {
+    console.log(`${name} wins`);
+    gameCompleted = true;
+    game.restart();
+}
+
 const gameDriver = {
-    start(root) {
-        game.init({
-            onEnterPlayerCreation: this.handleEnterPlayerCreation,
-            onPlayerChange: this.handlePlayerChange,
-            onEnterShipPlacements: this.handleEnterShipPlacements,
-            onEnterRound: this.handleEnterRound,
-            onDeclareWinner: this.handleDeclareWinner,
-        });
-
-        const gameContainer = root.querySelector(
-            "[data-role='game-container']",
-        );
-
-        gameView.init(gameContainer);
-        game.start();
-    },
-
-    handleEnterPlayerCreation() {
-        if (gameCompleted) {
-            console.log('Game restarted');
-            return;
-        }
-
-        gameView.showPlayerCreation();
-        game.submitPlayerCreation(true, 'P1', true, 'P2');
-    },
-
-    handlePlayerChange(name) {
-        console.log('Current player:', name);
-        player = name === 'P1' ? 1 : 2;
-    },
-
-    handleEnterShipPlacements(shipIds) {
-        console.log('Entered ship placements. Received:', shipIds);
-        game.placeShip(shipIds[0].id, 4, 3);
-        game.placeShip(shipIds[1].id, 4, 4);
-        game.placeShip(shipIds[2].id, 4, 5);
-        game.placeShip(shipIds[3].id, 4, 6);
-        game.placeShip(shipIds[4].id, 4, 7);
-        game.submitShipPlacements();
-    },
-
-    handleEnterRound() {
-        console.log('Entered round');
-
-        if (player === 1) {
-            y1 = x1 < 10 ? y1 : y1 + 1;
-            x1 = x1 < 10 ? x1 : 0;
-            console.log(`Submitting attack to (${x1}, ${y1})`);
-            game.submitAttack(x1++, y1);
-        } else {
-            y2 = x2 < 10 ? y2 : y2 + 1;
-            x2 = x2 < 10 ? x2 : 0;
-            console.log(`Submitting attack to (${x2}, ${y2})`);
-            game.submitAttack(x2++, y2);
-        }
-    },
-
-    handleDeclareWinner(name) {
-        console.log(`${name} wins`);
-        gameCompleted = true;
-        game.restart();
-    },
+    start,
 };
 
 export default gameDriver;
