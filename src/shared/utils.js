e3d27d1159718d9252152cf2c21a57e2ea9d1b2b
Simeon Rediger <simeonrediger@gmail.com>
2026-01-22 13:01:49 -0500

    Add getElementWidth() and getContentWidth()

--- src/shared/utils.js
+++ src/shared/utils.js
@@ -1,3 +1,5 @@
+import '@/shared/styles/utilities.css';
+
 export function adoptValuesOfCommonKeys(targetObj, sourceObj) {
     Object.keys(targetObj).forEach(key => (targetObj[key] = sourceObj[key]));
 }
@@ -6,6 +8,35 @@ export function create2dArray(rows, columns = rows, valueFunc) {
     return new Array(rows)
         .fill()
         .map(() => new Array(columns).fill().map(() => valueFunc?.()));
+}
+
+export function getContentWidth(element) {
+    return getElementWidth(element, false);
+}
+
+export function getElementWidth(element, includePadding = true) {
+    const originalDisplayValue = element.style.display;
+    const hasHiddenClass = element.classList.contains('hidden');
+
+    element.style.display = 'block';
+    element.classList.remove('hidden');
+    let width = element.getBoundingClientRect().width;
+
+    if (!includePadding) {
+        const inlinePadding = getComputedStyle(element)
+            .paddingInline.replaceAll('px', '')
+            .split(' ')
+            .reduce((sum, num) => (sum += +num), 0);
+
+        width = width - inlinePadding;
+    }
+
+    if (hasHiddenClass) {
+        element.classList.add('hidden');
+    }
+
+    element.style.display = originalDisplayValue;
+    return width;
 }
 
 export function validateElements(elements) {
