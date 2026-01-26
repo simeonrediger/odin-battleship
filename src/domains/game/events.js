346732f0389241c0d8e2a2bdf1e5eda3fc3e30d1
Simeon Rediger <simeonrediger@gmail.com>
2026-01-25 23:06:12 -0500

    Remove empty payload definitions

--- src/domains/game/events.js
+++ src/domains/game/events.js
@@ -1,10 +1,6 @@
 // Event payloads
 
 /**
- * @typedef {Object} EnteredPlayerCreationDetail
- */
-
-/**
  * @typedef {Object} EnteredShipPlacementsDetail
  * @property {string} playerName - Name of the current player.
  * @property {string} opponentName - Name of the current opponent.
@@ -19,7 +15,6 @@
  * @description Emitted upon entering the player creation phase.
  * @event entered-player-creation
  * @type {string}
- * @see {@link EnteredPlayerCreationDetail}
  */
 export const ENTERED_PLAYER_CREATION = 'entered-player-creation';
 
